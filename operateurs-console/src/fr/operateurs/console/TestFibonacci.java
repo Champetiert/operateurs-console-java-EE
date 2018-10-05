b477babc4d368db93054010db680a29f44ad7190
Champetier <todave11@gmail.com>
2018-10-05 14:25:57 +0200

    test

--- operateurs-console/src/fr/operateurs/console/TestFibonacci.java
+++ operateurs-console/src/fr/operateurs/console/TestFibonacci.java
@@ -3,41 +3,41 @@ package fr.operateurs.console;
 import java.util.Scanner;
 
 public class TestFibonacci {
-	
-	
+
 	/**
 	 * @param N Rang de la suite
 	 * @return le nombre de rang N dans une suite de Fibonacci
 	 */
 	static int calculeFibonacciRécursif(int N) {
-		if(N ==1) {
+		if (N == 1) {
 			return 0;
-		}else if (N==2) {
+		} else if (N == 2) {
 			return 1;
-		}else return calculeFibonacciRécursif(N-1)+calculeFibonacciRécursif(N-2);
+		} else
+			return calculeFibonacciRécursif(N - 1) + calculeFibonacciRécursif(N - 2);
 	}
-	
+
 	/**
-	 * @param N
-	 * @return
+	 * @param N Rang de la suite
+	 * @return le nombre de rang N dans une suite de Fibonacci
 	 */
 	static int calculeFibonacci(int N) {
-		if(N ==1) {
+		if (N == 1) {
 			return 0;
-		}else if (N==2) {
+		} else if (N == 2) {
 			return 1;
-		}else {
-			int u0=0;
-			int u1=1;
-			int val=u0+u1;
-			for(int i=4;i<=N;i++) {
-				u0=u1;
-				u1=val;
-				val=u0+u1;		
+		} else {
+			int u0 = 0;
+			int u1 = 1;
+			int val = u0 + u1;
+			for (int i = 4; i <= N; i++) {
+				u0 = u1;
+				u1 = val;
+				val = u0 + u1;
 			}
 			return val;
 		}
-		
+
 	}
 
 	/**
@@ -47,9 +47,8 @@ public class TestFibonacci {
 		System.out.println("Veuillez saisir le rang N de la suit de Fibonacci");
 		Scanner questionUser = new Scanner(System.in);
 		int a = questionUser.nextInt();
-		int result=calculeFibonacci(a);
+		int result = calculeFibonacci(a);
 		System.out.println(result);
-		
 
 	}
 

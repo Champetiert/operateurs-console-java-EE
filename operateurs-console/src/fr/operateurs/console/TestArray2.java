77377e19a4756204ef51ab6cbae9aff2899973c6
Champetier <todave11@gmail.com>
2018-10-05 15:51:26 +0200

    push de pause

--- operateurs-console/src/fr/operateurs/console/TestArray2.java
+++ operateurs-console/src/fr/operateurs/console/TestArray2.java
@@ -3,14 +3,22 @@ package fr.operateurs.console;
 public class TestArray2 {
 	static int[] array = { 1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4 };
 
-	static void moy() {
+	/**
+	 * Calcule de la moyenne des valeurs du tableau
+	 */
+	static void moy(int[] array) {
 		int sum = 0;
 		for (int i = 0; i < array.length; i++) {
-			sum += array[i];
+			sum += array[i]; // calcule de la sommes du tableaux
 		}
-		System.out.println((sum / array.length));
+		// Affichage du résultat et calcule de la moyenne avec la formule "sommes des
+		// éléments/ nombre d'élément"
+		System.out.println("la moyenne du tableau est " + (sum / array.length));
 	}
 
+	/**
+	 * Compte du nombre de doublon dans un tableau
+	 */
 	static void doublon() {
 		int doublon[] = new int[array.length / 2];
 		int cpt = 0;
@@ -39,9 +47,9 @@ public class TestArray2 {
 	}
 
 	public static void main(String[] args) {
-		// moy();
+		// moy(array);
 		// l'entié 15 a pour index 1
-		doublon();
+		// doublon();
 	}
 
 }
--- operateurs-console/src/fr/operateurs/console/TestArrayTri.java
+++ operateurs-console/src/fr/operateurs/console/TestArrayTri.java
@@ -7,9 +7,9 @@ public class TestArrayTri {
 	static int[] array = {1, 15, -3, 0, 8, 7, 4, -2, 28, 7, -1, 17, 2, 3, 0, 14, -4} ;
 
 	public static void main(String[] args) {
-		Arrays.sort(array);
-		for (int i = 0; i < array.length; i++) {
-			System.out.println(array[i]);
+		Arrays.sort(array);							//On Utilise la méthode "sort" de la Class Arrays qui try les nombres par ordre croissant
+		for (int i = 0; i < array.length; i++) { 	//On affiche le tableau
+			System.out.println(array[i]);			//
 		}
 	}
 

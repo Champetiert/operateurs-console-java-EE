2b01ff87671fd28a140e74e09f42dd875bf95848
Champetier <todave11@gmail.com>
2018-10-08 10:16:47 +0200

    Fin des commentaires TestArray1

--- operateurs-console/src/fr/operateurs/console/TestArray1.java
+++ operateurs-console/src/fr/operateurs/console/TestArray1.java
@@ -8,7 +8,7 @@ public class TestArray1 {
 	 * Affiche le tableau du plus peutit index au plus grand
 	 */
 	static void increment() {
-		for (int i = 0; i < array.length; i++) {
+		for (int i = 0; i < array.length; i++) { //boucle de lecture du tableau de longueur array.length
 			System.out.println(array[i]);
 		}
 	}
@@ -17,7 +17,7 @@ public class TestArray1 {
 	 * Affiche le tableau du plus grand index au plus petit
 	 */
 	static void decrement() {
-		for (int i = array.length - 1; i >= 0; i--) {
+		for (int i = array.length - 1; i >= 0; i--) {//boucle de lecture inverse du tableau de longueur array.length
 			System.out.println(array[i]);
 		}
 	}
@@ -26,8 +26,8 @@ public class TestArray1 {
 	 * Affiche les valeurs du tableau qui sont > à 3
 	 */
 	static void supTroi() {
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] > 3) {
+		for (int i = 0; i < array.length; i++) {//boucle de lecture du tableau de longueur array.length
+			if (array[i] > 3) {					//condition affichage valeur array a indice i > à 3
 				System.out.println(array[i]);
 			}
 		}
@@ -37,8 +37,8 @@ public class TestArray1 {
 	 * Affiche les valeurs du tableau qui sont paires
 	 */
 	static void pairs() {
-		for (int i = 0; i < array.length; i++) {
-			if (array[i] % 2 == 0) {
+		for (int i = 0; i < array.length; i++) {//boucle de lecture du tableau de longueur array.length
+			if (array[i] % 2 == 0) {			//condition affichage valeur array a indice i quand son modulo = 0
 				System.out.println(array[i]);
 			}
 		}
@@ -48,10 +48,10 @@ public class TestArray1 {
 	 * Affiche la valeur Max du tableau
 	 */
 	static void max() {
-		int max = array[0];
-		for (int i = 2; i < array.length; i++) {
-			if (array[i] > max) {
-				max = array[i];
+		int max = array[0];							//initialise le max à la première valeur du tableau
+		for (int i = 1; i < array.length; i++) {	//boucle de lecture du tableau de longueur array.length
+			if (array[i] > max) {					//test si la valeur array à l'indice i est > a la valeur max
+				max = array[i];						//si vrais, alors on change la valeur max
 			}
 		}
 		System.out.println(max);
@@ -61,10 +61,10 @@ public class TestArray1 {
 	 * Affiche la valeur Min du tableau
 	 */
 	static void min() {
-		int min = array[0];
-		for (int i = 2; i < array.length; i++) {
-			if (array[i] < min) {
-				min = array[i];
+		int min = array[0];							//initialise le min à la première valeur du tableau
+		for (int i = 2; i < array.length; i++) {	//boucle de lecture du tableau de longueur array.length
+			if (array[i] < min) {					//test si la valeur array à l'indice i est < a la valeur min
+				min = array[i];						//si vrais, alors on change la valeur min
 			}
 		}
 		System.out.println(min);
